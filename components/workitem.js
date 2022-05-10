754fd874fdefc73bf4913bd6ed1976ab7dbc6a39
Adil Shaikh <m.adilshaikh1@gmail.com>
2022-05-10 12:59:48 +0500

    relift

--- components/workitem.js
+++ components/workitem.js
@@ -2,34 +2,34 @@ import Preview from './preview'
 
 const WorkItem = ({ item }) => (
   <section className={'work-item'} style={item.theme}>
-  <header>
-    <h1 className={'title'}>{item.title}</h1>
-    <p className="short">{item.short}</p>  
-  </header>
-  <Preview screens={item.previews}/>
-  <div className="information">
-    <div className="specs">
-      <p><span className="label">Role</span>{item.role}</p>
-      <p><span className="label">Status</span>{item.status}</p>
-      <br/>
-      <p className="date">{item.date}</p>
-    </div>
-    <div className="details">
-      <p>{item.details}</p>
-      <div className="tech">
-        <p className="label">Technology Stack</p>
-        <div className="items">
-        {
-          item.technologies.map(tech => (
-            <div className="item">
-              <img src={'/static/img/logos/'+tech.replace(' ', '').toLowerCase() + '.png'}/>
-            </div>
-          ))
-        }
-        </div>  
+    <div className="date center">{item.date}</div>
+    <header>
+      <h1 className={'title'}>{item.title}</h1>
+      <p className="short">{item.short}</p>  
+    </header>
+    <Preview screens={item.previews}/>
+    <div className="information">
+      <div className="specs">
+        <p><span className="label">Role</span>{item.role}</p>
+          <p><span className="label">Status</span>{item.status} {item.link ? <a href={item.link} target="_blank">🔗</a> : ''}</p>
+        <br/>
+        
+      </div>
+      <div className="details">
+        <p>{item.details}</p>
+        <div className="tech">
+          <div className="items">
+          {
+            item.technologies.map(tech => (
+              <div className="item">
+                <img src={'/static/img/logos/'+tech.replace(' ', '').toLowerCase() + '.png'}/>
+              </div>
+            ))
+          }
+          </div>  
+        </div>
       </div>
     </div>
-  </div>
   </section>
   )
 
--- pages/index.js
+++ pages/index.js
@@ -10,93 +10,89 @@ class Index extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			screenshot: {
-				type: 'desktop'
-			},
-      title: 'promptdog',
-      siteTitle: 'Adil Shaikh - Design and Product Development',
+      siteTitle: 'Adil Shaikh - Product and Engineering',
       description: 'Design to engineering to validation to commercial product to value.',
-			items: [
+      items: [
 			{
 				title: 'TestPrepEngine',
-				theme: {backgroundColor: '#00d4b6', color: '#fff'},
+				theme: {backgroundColor: '#2d8a7c', color: '#fff'},
 				foreColor: '#efefef',
-				short: 'Test Preparation with customized testing and performance insights',
-				date: 'February 2019',
+				short: 'Student Test Preparation with customized testing and performance insights',
+				date: '2018-2019',
 				previews: [
 				{img_url: '/static/img/tpe.gif'}
 				],
 				role: 'Full-Stack Development',
-				status: 'Complete',
-				details: 'UI brought to life. TestPrepEngine VueJS app is powered by API Backend featuring testing with custom questions and time limits. Also delivering performance insights based on test results.',
+				status: 'Public Good till \'21',
+				details: 'UI brought to life. TestPrepEngine VueJS app is powered by API Backend featuring testing with custom questions and time limits. Also delivering drill-across performance insights based on user test results.',
 				technologies: ['nodejs', 'vuejs', 'sass']
-			},
+      },
+      {
+        title: 'BMoreRaw > RawResearch',
+        theme: {backgroundColor: '#060730', color: '#fff'},
+        foreColor: '#efefef',
+        short: 'Market research platform for businesses connecting with their customers',
+        date: '2019',
+        previews: [
+          { img_url: '/static/img/rawresearch-1.jpg' },
+          { img_url: '/static/img/rawresearch-2.jpg' },
+          { img_url: '/static/img/rawresearch-3.jpg' }
+        ],
+        role: 'Rapid Concept Design',
+        status: 'Evolved @ RawResearch',
+        link: 'https://rawresearch.io/',
+        details: 'Formerly called BMoreRaw, I engaged with its founder alongside my DevShop partner to process specifications, turning them to rapid visuals which were prototyped to be functional as I handed engineering schemas over to a pair of brilliant engineers. Ideas to concept to prototype eased the communication with engineers and 3rd designers for end-user UX, turning prospects to customers.',
+        technologies: ['figma', 'flask', 'reactjs']
+      },
 			{
-				title: 'TelePrompter',
-				theme: {backgroundColor: '#2c3e50', color: '#fff'},
+				title: 'PromptDog TelePrompter',
+				theme: {backgroundColor: '#000', color: '#fff'},
 				short: 'Cross-Platform teleprompter with complete feature set',
-				date: 'July 2018',
+				date: '2018',
 				previews: [
-				{img_url: '/static/img/teleprompt.png'},
-				{img_url: '/static/img/teleprompt.png'}
+				{img_url: '/static/img/promptdog-1.jpg'}
 				],
-				role: 'UI Design & Development',
-				status: 'Proprietary',
-				details: 'Built for performance on all desktop platforms using electron and quasar framework (Vue.js). The teleprompter was built ground up to support extensive features not limited to formatting but also mirroring the prompt, managing profiles as well as creating and editing the text using a powerful WYSWIG editor within the application',
+				role: 'Cross-Platform App Development',
+        status: 'Commercialized @ PromptDog as V4',
+        link: "https://promptdog.com/",
+				details: 'Built for performance supporting all desktop platforms using electron and quasar framework (Vue.js). The teleprompter was built ground up with extensive features not limited to formatting but also mirroring the prompt, managing visual profiles as well as creating and editing the text using a powerful WYSWIG editor within the application.',
 				technologies: ['electron', 'quasar', 'vuejs', 'sass']
 			},
 			{
 				title: 'Engro Systems',
 				theme: {
 					backgroundColor: '#222222',
-					color: '#fff'	
+					color: '#fff'
 				},
 				short: 'Automating recruitment, scheduled reporting and documentation processes',
-				date: 'July 2016',
+				date: '03/2017 - 09/2017',
 				previews: [
 				{img_url: '/static/img/engrosystems-1.jpg'},
 				{img_url: '/static/img/engrosystems-2.jpg'},  
         {img_url: '/static/img/engrosystems-3.jpg'},
 				],
-				role: 'Product Development',
-				status: 'Proprietary',
-				details: 'EngroSystems is a tailored HR automation system for recruitment, intern management and timely generation and delivery of reports and certificates across various organizational deparments.',
+				role: 'UI / UX & Product Development',
+				status: 'Founding Product @ Partnered DevShop',
+				details: 'Engro Systems is a tailored HR automation system for recruitment, intern management and timely generation and delivery of reports and certificates across various organizational deparments. I used my well-perfected craft of turning mental models to functional UI directly in-code, quickly building a rapid delivery - feedback - iterations loop. In extension I branded it strategically to pitch an extendable business case to decision makers.',
 				technologies: ['mysql', 'flask', 'vuejs', 'semanticui', 'sass']
 			},
       {
         title: 'UX-App',
         short: 'Full featured web based UI builder',
         theme: {
-          backgroundColor: '#111',
+          backgroundColor: '#3c5e57',
           color: '#fff'
         },
         previews: [
         {img_url: '/static/img/uxapp.png'},
         ],
-        role: 'UI Design',
-        status: 'Evolved',
-        date: 'August 2016',
-        details: 'Early concept design for a new product featuring a host of prebuilt UI components.',
+        role: 'Concept UI Design',
+        status: 'Evolved @ UX-App',
+        link: 'https://www.ux-app.com/',
+        date: '2016',
+        details: 'Early concept design for productization of extensive UI components built by a teacher. The mockups motivated new co-founder to extend a substantial equity offer for full-time commitment, as he was set on to commercialize it as a B2B UI tool. Despite passing on the offer, observing the trajectory of UX-App and founder dynamics made it my earliest motivation to go all in with product-engineering-market confluence.',
         technologies: ['photoshop', 'illustrator']
       },
-			{
-				title: 'BlueCollar',
-				theme: {
-					backgroundColor: '#351F77',
-					color: '#fff'
-				},
-				color: '#3498db',
-				short: 'A platform for getting quick blue collar help at your doorstep',
-				date: 'July 2017',
-				previews: [
-				{img_url: '/static/img/bluecollar-1.png'},
-				{img_url: '/static/img/bluecollar-2.png'},
-				{img_url: '/static/img/bluecollar-3.png'},
-				],
-				role: 'Brand Identity & Web Design',
-				status: 'Retired',
-				technologies: ['html5', 'css3']
-			},
 			{
 				title: 'Medizin Medien',
 				theme: {
@@ -105,13 +101,14 @@ class Index extends Component {
 				},
 				color: '#3498db',
 				short: 'Foundation to medical industry publications',
-				date: 'June 2016',
+				date: '2016',
 				previews: [
 				{img_url: '/static/img/medizin-1.jpg'},
 				{img_url: '/static/img/medizin-2.jpg'},
 				],
 				role: 'Wordpress Theme Development',
-				status: 'Evolved',
+        status: 'Diversified @ MedTrix Group',
+        link: 'https://medtrix.group/oesterreich/',
 				'details': 'Built on then modern WP roots stack, Medizin Medien custom WP theme was given support for 3 distinct custom post types and Visual Composer (WPBakery Site Builder) custom components.',
 				technologies: ['wordpress'],
 				demo: 'https://medizin-medien.at/',
@@ -125,10 +122,9 @@ class Index extends Component {
 	}
 
 	render() {
-// <p>Artist at core <br/> Developer by profession <br/> Disguised as a Computer Scientist</p>
-// <img src="/static/img/logo.svg" style={{height: 50, margin: '30px 0 0 30px'}} alt=""/>
 return(
   <div>
+
   	<Head>
       <title>{this.state.siteTitle}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
@@ -138,16 +134,17 @@ return(
       <meta name="description" content={this.state.description} />
       <meta property='og:description' content={this.state.description} />
       <meta name='twitter:description' content={this.state.description} />
-  	</Head>
+    </Head>
+    
     <section className="intro">
 
       <div className="catch">
         <p class="bigs">
           <span style={{color: '#f45'}}>Artist at core.</span><br/>
-          <span style={{color: '#929292'}}>Developer by profession.</span><br/>
-          <span style={{color: '#bfbfbf'}}>Architect in disguise.</span>
+          <span style={{color: '#929292'}}>Engineer by trade<span style={{color: '#ffdf00'}}>.</span></span><br/>
+          <span style={{color: '#bfbfbf'}}>Harbinger of purpose<span style={{color: '#0eff00'}}>.</span></span>
         </p>
-        <p>... and bit of a knack for 🌏 🌍 🌎 📈 .. 👽</p>
+        <p>... with a bit of a knack for 🌏 🌍 🌎, 📈 &amp; 👽</p>
       </div>
 
       <div className="me">
@@ -160,17 +157,32 @@ return(
           </div>
         </div>
       </div>
+
+      <big>👇 <strong>My commercial contributions</strong> making the bedrock of successful* products!</big>
+      <small>* for there is no failure that doesn't yield success!</small>
     </section>
     {this.renderItems()}
-    <section class="call center">
-		
-		<a class="button bold medium" target="_blank" href="https://www.dropbox.com/sh/bl1un7pb2d99cgr/AADjUjToYVWGBDz-Pb9byiTua?dl=0">VISIT DESIGN ARCHIVES</a>
 
-  		<h1>I am up for a chat!</h1>
+    <section class="call center">
+      
+      <code>Formerly, A <em>Brand Identity / Marketing / UX</em> Designer (2010-16)</code>
+
+      <br/><br/>
+
+      <a class="button bold large" target="_blank" href="https://www.dropbox.com/sh/bl1un7pb2d99cgr/AADjUjToYVWGBDz-Pb9byiTua?dl=0">VISIT MY BRANDING &amp; DESIGN GALLERY</a>
+
+      <br/><br/><br/>
+      <p>Collaborating professionally for over a decade across 🌐 specifically 🇺🇸 🇨🇦 🇬🇧 🇦🇹 🇩🇰</p>
+      
+      <h1>Want to realize your targets?</h1>
+      <big>I go beyond engineering.. reach out to learn more from my failures alike!</big>
   		<div class="socials">
-       		<a target="_blank" href="mailto:hello@adils.me"><img height="32" src="/static/img/email.svg" /> <span>hello@adils.me</span></a>
-      	</div>
-  	</section>
+       	<a target="_blank" href="mailto:hello@adils.me"><img height="48" src="/static/img/email.svg" /> <span>hello@adils.me</span></a>
+      </div>
+    </section>
+    <footer>
+      &copy; 2022 Adils.
+    </footer>
   </div>
 	)}
 }
